8109a923986b135ebd6e0213c9ca907d65d9f1b8
Kelvin Chan <kelvin.chan273@gmail.com>
2019-08-31 13:08:07 -0400

    Add Project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Import the react and react dom libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a react component
+const App = function() {
+    return ()
+        <div>
+            <label class = "label" for="name">Enter name:</label>
+            <input id="name" type = "text"/>
+            <button style = "background-color: blue; color:white">Submit</button>
+        </div>
+    );
+}
+
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root'));
